8264247c4c97b86af86f306028fbf64121a6a90c
Shaun Priestley <shaun@cksm.cc>
2022-03-25 21:04:42 +1100

    Added decode_quantum() to base64
    This converts 4 base64 input bytes into 3 data bytes. Essentially the reverse of encode_bytes()
    
    A test was added as appropriate, and the base64::tests module was modified to store test values in const's

--- src/base64.rs
+++ src/base64.rs
@@ -31,13 +31,49 @@ fn encode_quantum(input: [u8; 3]) -> [u8; 4] {
     [c1, c2, c3, c4]
 }
 
+fn decode_quantum(input: [u8; 4]) -> [u8; 3] {
+    let d1 = (
+        input[0] << 2
+    ) | (
+        input[1] >> 4 &
+        0b00000011
+    );
+
+    let d2 = (
+        input[1] << 4
+    ) | (
+        input[2] >> 2 &
+        0b00001111
+    );
+
+    let d3 = (
+        input[2] << 6
+    ) | (
+        input[3] &
+        0b00111111
+    );
+
+    [d1, d2, d3]
+}
+
 #[cfg(test)]
 mod tests {
     use crate::base64;
+
+    const QUANTUM_BINARY: [u8; 3] = [0b00000100, 0b00010000, 0b01000001];
+    const QUANTUM_BASE64: [u8; 4] = [1, 1, 1, 1];
+    
     #[test]
     fn encode_quantum_validation() {
-        let input: [u8; 3] = [0b00000100, 0b00010000, 0b01000001];
+        let input = QUANTUM_BINARY;
         let output = base64::encode_quantum(input);
-        assert_eq!(output, [1, 1, 1, 1]);
+        assert_eq!(output, QUANTUM_BASE64);
+    }
+
+    #[test]
+    fn decode_quantum_validation() {
+        let input = QUANTUM_BASE64;
+        let output = base64::decode_quantum(input);
+        assert_eq!(output, QUANTUM_BINARY);
     }
 }
