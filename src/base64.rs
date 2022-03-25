e479197d20c0e2598085dca8fc9485c63433e066
Shaun Priestley <shaun@cksm.cc>
2022-03-25 21:23:34 +1100

    Added encode_partial_16() to base64
    This will encode 2 octects into 3 base64 characters. Needed for processing the remaining 16 bits at the end of a 64 bit ID
    
    Test added as appropriate

--- src/base64.rs
+++ src/base64.rs
@@ -31,6 +31,21 @@ fn encode_quantum(input: [u8; 3]) -> [u8; 4] {
     [c1, c2, c3, c4]
 }
 
+fn encode_partial_16(input: [u8; 2]) -> [u8; 3] {
+    let c1 = input[0] >> 2;
+
+    let c2 = (
+        input[1] >> 4
+    ) | (
+        input[0] << 4 &
+        0b00110000
+    );
+    
+    let c3 = input[1] << 2 & 0b00111100;
+
+    [c1, c2, c3]
+}
+
 fn decode_quantum(input: [u8; 4]) -> [u8; 3] {
     let d1 = (
         input[0] << 2
@@ -62,12 +77,21 @@ mod tests {
 
     const QUANTUM_BINARY: [u8; 3] = [0b00000100, 0b00010000, 0b01000001];
     const QUANTUM_BASE64: [u8; 4] = [1, 1, 1, 1];
+
+    const PARTIAL_16_BINARY: [u8; 2] = [0b00000100, 0b00010001];
+    const PARTIAL_16_BASE64: [u8; 3] = [1, 1, 4];
     
     #[test]
     fn encode_quantum_validation() {
         let input = QUANTUM_BINARY;
         let output = base64::encode_quantum(input);
         assert_eq!(output, QUANTUM_BASE64);
+    }
+
+    #[test]
+    fn encode_partial_16_validation() {
+        let output = base64::encode_partial_16(PARTIAL_16_BINARY);
+        assert_eq!(output, PARTIAL_16_BASE64);
     }
 
     #[test]
