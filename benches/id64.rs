c8c70c656da47fa8cf6d9d3d62feceff35838875
Shaun Priestley <shaun@cksm.cc>
2022-08-28 12:51:23 +1000

    Added i64 benchmarks alongside u64 counterparts

--- benches/id64.rs
+++ benches/id64.rs
@@ -11,5 +11,20 @@ fn decode_u64_bench(c: &mut Criterion) {
     c.bench_function("decode Id64 as u64", |b| b.iter(|| black_box(u64::from(id))));
 }
 
-criterion_group!(benches, encode_u64_bench, decode_u64_bench);
+fn encode_i64_bench(c: &mut Criterion) {
+    c.bench_function("encode i64 as Id64", |b| b.iter(|| black_box(Id64::from(25519i64))));
+}
+
+fn decode_i64_bench(c: &mut Criterion) {
+    let id: Id64 = Id64::from(25519i64);
+    c.bench_function("decode Id64 as i64", |b| b.iter(|| black_box(u64::from(id))));
+}
+
+criterion_group!(
+    benches,
+    encode_u64_bench,
+    decode_u64_bench,
+    encode_i64_bench,
+    decode_i64_bench,
+);
 criterion_main!(benches);
