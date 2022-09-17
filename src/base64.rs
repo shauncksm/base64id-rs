29cd4634972539ff1c45b15794ed9a0b3df8e13d
Shaun Priestley <shaun@cksm.cc>
2022-09-17 23:41:47 +1000

    Clippy. Removed redundant closure

--- src/base64.rs
+++ src/base64.rs
@@ -39,7 +39,7 @@ pub fn decode_i64(input: [char; 11]) -> Result<i64, Error> {
             .find(input[i])
             .ok_or(Error::InvalidCharacter)?;
 
-        c[i] = u8::try_from(idx).map_err(|e| Error::InfallibleU8FromUsize(e))?;
+        c[i] = u8::try_from(idx).map_err(Error::InfallibleU8FromUsize)?;
     }
 
     let p1 = decode_quantum([c[0], c[1], c[2], c[3]]);
