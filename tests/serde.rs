be4b6e7aee06d69bc54e41d5063266965ea6d21b
Shaun Priestley <shaun@cksm.cc>
2022-11-19 13:06:43 +1100

    Moved serde Id64 tests into macro

--- tests/serde.rs
+++ tests/serde.rs
@@ -1,30 +1,42 @@
-use serde::{Deserialize, Serialize};
+macro_rules! generate_serde_test_suite {
+    ($lib_type:ident, $lib_type_name:ident, $i_type:ident, $test_int:literal, $test_str:literal) => {
+        #[cfg(feature = "serde")]
+        mod $lib_type_name {
+            use base64id::$lib_type;
+            use serde::{Deserialize, Serialize};
 
-use base64id::Id64;
+            const INT: $i_type = $test_int;
+            const JSON: &str = $test_str;
 
-const INT: i64 = 2063772195469131459;
-const JSON: &str = r#"{"id":"HKP94KBD_sM"}"#;
+            #[derive(Serialize, Deserialize)]
+            struct Record {
+                pub id: $lib_type,
+            }
 
-#[derive(Serialize, Deserialize)]
-struct Record {
-    pub id: Id64,
-}
+            #[test]
+            fn serde_serialize() {
+                let record = Record {
+                    id: $lib_type::from(INT),
+                };
+                let encoded = serde_json::to_string(&record).unwrap();
 
-#[test]
-#[cfg(feature = "serde")]
-fn serde_id64_serialize() {
-    let record = Record {
-        id: Id64::from(INT),
+                assert_eq!(JSON, encoded);
+            }
+
+            #[test]
+            fn serde_deserialize() {
+                let decoded: Record = serde_json::from_str(JSON).unwrap();
+
+                assert_eq!(INT, $i_type::from(decoded.id));
+            }
+        }
     };
-    let encoded = serde_json::to_string(&record).unwrap();
-
-    assert_eq!(JSON, encoded);
 }
 
-#[test]
-#[cfg(feature = "serde")]
-fn serde_id64_deserialize() {
-    let decoded: Record = serde_json::from_str(JSON).unwrap();
-
-    assert_eq!(INT, i64::from(decoded.id));
-}
+generate_serde_test_suite!(
+    Id64,
+    id64,
+    i64,
+    2063772195469131459,
+    r#"{"id":"HKP94KBD_sM"}"#
+);
