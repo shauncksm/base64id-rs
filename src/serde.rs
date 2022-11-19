67900073a05244a9ad8b4b932d539be427338106
Shaun Priestley <shaun@cksm.cc>
2022-11-19 12:52:26 +1100

    Moved serde impls into macro
    A new generate_serde_trait_impls macro can be used to generate serde Serialize and Deserialize impls for Id64, as well as future Id32 and Id16 types

--- src/serde.rs
+++ src/serde.rs
@@ -7,51 +7,63 @@ use serde::{
 
 use crate::{error::Error, Id64};
 
-impl Serialize for Id64 {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.collect_str(self)
-    }
+macro_rules! generate_serde_trait_impls {
+    ($lib_type:ident, $visitor_type:ident, $char_count:literal, $last_char_range:literal) => {
+        impl Serialize for $lib_type {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                serializer.collect_str(self)
+            }
+        }
+
+        impl<'de> Deserialize<'de> for $lib_type {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                deserializer.deserialize_str($visitor_type)
+            }
+        }
+
+        struct $visitor_type;
+
+        impl<'de> Visitor<'de> for $visitor_type {
+            type Value = $lib_type;
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(
+                    f,
+                    concat!($char_count, " character base64url encoded string")
+                )
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                const EXP1: &str = concat!("exactly ", $char_count, " base64url characters");
+                const EXP2: &str = concat!(
+                    "the last character must be one of the following: ",
+                    $last_char_range
+                );
+
+                $lib_type::from_str(v).map_err(|e| match e {
+                    Error::InvalidLength => E::invalid_length(v.len(), &EXP1),
+                    Error::InvalidCharacter => E::invalid_value(
+                        Unexpected::Other("1 or more non-base64url characters"),
+                        &EXP1,
+                    ),
+                    Error::OutOfBoundsCharacter => E::invalid_value(
+                        Unexpected::Other("the last character was out of bounds"),
+                        &EXP2,
+                    ),
+                    Error::InfallibleU8FromUsize(_) => E::custom(e),
+                })
+            }
+        }
+    };
 }
 
-impl<'de> Deserialize<'de> for Id64 {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(Id64Visitor)
-    }
-}
-
-struct Id64Visitor;
-
-impl<'de> Visitor<'de> for Id64Visitor {
-    type Value = Id64;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "11 character base64url encoded string")
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        const EXP1: &str = "exactly 11 base64url characters";
-        const EXP2: &str = "the last character must be one of the following: AEIMQUYcgkosw048";
-
-        Id64::from_str(v).map_err(|e| match e {
-            Error::InvalidLength => E::invalid_length(v.len(), &EXP1),
-            Error::InvalidCharacter => E::invalid_value(
-                Unexpected::Other("1 or more non-base64url characters"),
-                &EXP1,
-            ),
-            Error::OutOfBoundsCharacter => E::invalid_value(
-                Unexpected::Other("the last character was out of bounds"),
-                &EXP2,
-            ),
-            Error::InfallibleU8FromUsize(_) => E::custom(e),
-        })
-    }
-}
+generate_serde_trait_impls!(Id64, Id64Visitor, 11, "AEIMQUYcgkosw048");
