21ee1ebb564efdeb7b89bef7e720f6050fd3df4f
Shaun Priestley <shaun@cksm.cc>
2022-11-19 14:07:17 +1100

    Updated random_sample example for Id32 values

--- examples/random_sample.rs
+++ examples/random_sample.rs
@@ -1,20 +1,45 @@
 //! Print a minimal table with random Id64 and corosponding i64 pairs
 //!
-//! Run example with `cargo run --example random_sample`
+//! Run example with `cargo run --example random_sample` for 64 bit values
+//!
+//! Run example with `cargo run --example random_sample 32` for 32 bit values
+
+use std::env::args;
 
 use rand::random;
 
-use base64id::Id64;
+use base64id::{Id32, Id64};
 
 fn main() {
-    println!("base64url    i64                   u64");
-    println!("-----------  --------------------  --------------------");
+    let default = String::from("64");
+    let args: Vec<String> = args().collect();
 
-    for _ in 0..10 {
-        let i64: i64 = random();
-        let id = Id64::from(i64);
-        let u64 = u64::from(id);
+    let id_type = args.get(1).unwrap_or(&default);
 
-        println!("{id}  {i64:>20}  {u64:>20}");
+    match id_type.as_str() {
+        "32" => {
+            println!("base64url   i32           u32");
+            println!("---------   -----------   ----------");
+
+            for _ in 0..10 {
+                let i32: i32 = random();
+                let id = Id32::from(i32);
+                let u32 = u32::from(id);
+
+                println!("{id}  {i32:>15}  {u32:>11}");
+            }
+        }
+        _ => {
+            println!("base64url    i64                   u64");
+            println!("-----------  --------------------  --------------------");
+
+            for _ in 0..10 {
+                let i64: i64 = random();
+                let id = Id64::from(i64);
+                let u64 = u64::from(id);
+
+                println!("{id}  {i64:>20}  {u64:>20}");
+            }
+        }
     }
 }
