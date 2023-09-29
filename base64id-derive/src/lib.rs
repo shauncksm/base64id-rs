e171d8d8e7277397ae5843e26cd236b57b27fa25
Shaun Priestley <shaun@cksm.cc>
2023-09-29 16:15:29 +1000

    Expanded derive to differentiate between int sizes

--- base64id-derive/src/lib.rs
+++ base64id-derive/src/lib.rs
@@ -7,13 +7,69 @@ pub fn tuple_struct_into_base64id(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).expect("failed to parse token stream");
 
     let ident = ast.ident;
+    let struct_type = get_validated_struct_data(ast.data);
 
-    quote::quote! {
-        impl ::core::fmt::Display for #ident {
-            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-                ::core::write!(f, "AAAAAAAAAAA")
+    match struct_type.to_string().as_str() {
+        "i64" => {
+            quote::quote! {
+                impl ::core::fmt::Display for #ident {
+                    fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                        ::core::write!(f, "AAAAAAAAAAA")
+                    }
+                }
             }
         }
+        "i32" => {
+            quote::quote! {
+                impl ::core::fmt::Display for #ident {
+                    fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                        ::core::write!(f, "AAAAAA")
+                    }
+                }
+            }
+        }
+        "i16" => {
+            quote::quote! {
+                impl ::core::fmt::Display for #ident {
+                    fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                        ::core::write!(f, "AAA")
+                    }
+                }
+            }
+        }
+        _ => panic!("invalid type within tuple struct, expected i64, i32 or i16"),
     }
     .into()
+}
+
+fn get_validated_struct_data(data: syn::Data) -> syn::Ident {
+    let data = match data {
+        syn::Data::Struct(s) => s,
+        _ => panic!("unsupported data type. expected a tuple struct"),
+    };
+
+    let fields = match data.fields {
+        syn::Fields::Unnamed(f) => f.unnamed,
+        _ => panic!("unsupported data type. expected a tuple struct"),
+    };
+
+    let item = match fields.len() {
+        1 => fields.first().unwrap(),
+        _ => panic!("expected a tuple struct with exactly 1 field"),
+    };
+
+    let item_path = match item.ty.clone() {
+        syn::Type::Path(p) => p.path,
+        _ => panic!("invalid type within tuple struct, expected i64, i32 or i16"),
+    };
+
+    let item_type = match item_path.get_ident() {
+        Some(t) => t,
+        None => panic!("invalid type within tuple struct, expected i64, i32 or i16"),
+    };
+
+    match item_type.to_string().as_str() {
+        "i64" | "i32" | "i16" => return item_type.clone(),
+        _ => panic!("invalid type within tuple struct, expected i64, i32 or i16"),
+    };
 }
--- tests/derive.rs
+++ tests/derive.rs
@@ -8,3 +8,21 @@ fn id64_str_from_struct() {
     let id = MyId64(0);
     assert_eq!("AAAAAAAAAAA", format!("{id}"));
 }
+
+#[derive(Base64Id)]
+struct MyId32(i32);
+
+#[test]
+fn id32_str_from_struct() {
+    let id = MyId32(0);
+    assert_eq!("AAAAAA", format!("{id}"));
+}
+
+#[derive(Base64Id)]
+struct MyId16(i16);
+
+#[test]
+fn id16_str_from_struct() {
+    let id = MyId16(0);
+    assert_eq!("AAA", format!("{id}"));
+}
