0444e830963283071f31f8c1fc5aa79b6bfcda30
Shaun Priestley <shaun@cksm.cc>
2024-01-20 11:43:41 +1100

    Removed legacy type references from Error docs

--- base64id-core/src/error.rs
+++ base64id-core/src/error.rs
@@ -9,26 +9,23 @@ pub enum Error {
     /// returned when input data contains an invalid number of characters
     ///
     /// ## Expected Lengths
-    /// - `Id64` 11 characters
-    /// - `Id32` 6 characters
-    /// - `Id16` 3 characters
+    /// - `i64` 11 characters
+    /// - `i32` 6 characters
+    /// - `i16` 3 characters
     InvalidLength,
     /// returned when input data conatins a character that is not within the base64url alphabet
     InvalidCharacter,
     /// returned when the last character of input data is out of bounds
     ///
-    /// For `Id64` and `Id16` values, the last character must be one of the following:
+    /// For `i64` and `i16` values, the last character must be one of the following:
     /// ```txt
     /// AEIMQUYcgkosw048
     /// ```
     ///
-    /// For `Id32` values, the last character must be one of the following:
+    /// For `i32` values, the last character must be one of the following:
     /// ```txt
     /// AQgw
     /// ```
-    ///
-    /// ## Example
-    /// TBA
     OutOfBoundsCharacter,
 }
 
