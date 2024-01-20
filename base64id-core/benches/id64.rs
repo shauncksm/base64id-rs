30d575e8a276c35fa0f46a6a6827095cc02c11a8
Shaun Priestley <shaun@cksm.cc>
2024-01-20 11:24:47 +1100

    Updated benchmark to directly test encode/decode functions

--- base64id-core/benches/id64.rs
+++ base64id-core/benches/id64.rs
@@ -1,38 +1,22 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use base64id_core::Id64;
-
-fn encode_u64_bench(c: &mut Criterion) {
-    c.bench_function("encode u64 as Id64", |b| {
-        b.iter(|| black_box(Id64::from(25519u64)))
-    });
-}
-
-fn decode_u64_bench(c: &mut Criterion) {
-    let id: Id64 = Id64::from(25519u64);
-    c.bench_function("decode Id64 as u64", |b| {
-        b.iter(|| black_box(u64::from(id)))
-    });
-}
+use base64id_core::base64;
 
 fn encode_i64_bench(c: &mut Criterion) {
-    c.bench_function("encode i64 as Id64", |b| {
-        b.iter(|| black_box(Id64::from(25519i64)))
+    c.bench_function("encode i64 as [char; 11]", |b| {
+        b.iter(|| black_box(base64::encode_i64(25519i64)))
     });
 }
 
 fn decode_i64_bench(c: &mut Criterion) {
-    let id: Id64 = Id64::from(25519i64);
-    c.bench_function("decode Id64 as i64", |b| {
-        b.iter(|| black_box(u64::from(id)))
+    c.bench_function("decode [char; 11] as i64", |b| {
+        b.iter(|| {
+            black_box(base64::decode_i64([
+                'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'Y', '6', '8',
+            ]))
+        })
     });
 }
 
-criterion_group!(
-    benches,
-    encode_u64_bench,
-    decode_u64_bench,
-    encode_i64_bench,
-    decode_i64_bench,
-);
+criterion_group!(benches, encode_i64_bench, decode_i64_bench,);
 criterion_main!(benches);
